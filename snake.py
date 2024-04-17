a2f53f28147d524b1cfe8a462229f82193329b2f
Wilton Endril <wiltonendril@gmail.com>
2024-04-17 14:19:33 -0300

    adicionando parte de derrota

--- snake.py
+++ snake.py
@@ -1,20 +1,24 @@
 import curses
 
 def gameLoop(window):
+    curses.curs_set(0)
+    window.border(0)
+
     personagem = [10, 15]
     window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
     while True:
         window.timeout(1000)
         char = window.getch()
         window.cls()
+        window.border(0)
         match char:
-            case curses.keyUp:
+            case curses.KEY_UP:
                 personagem[0] -= 1 
-            case curses.keyLeft:
+            case curses.KEY_LEFT:
                 personagem[1] -= 1 
-            case curses.keyDown:
+            case curses.KEY_DOWN:
                 personagem[0] += 1
-            case curses.keyRight:
+            case curses.KEY_RIGHT:
                 personagem[1] += 1 
             case _: #não apertou a tecla ou n apertou a tecla correta.
                 pass
